022930abf2d89f26a2c858e94fa0c25dc8b786f9
lnlp <chadg1212@naver.com>
2025-05-12 23:29:34 +0900

    feat :: 기능 추가 및 구현

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ProfileCard from './components/ProfileCard';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-black flex items-center justify-center relative overflow-hidden">
+      <ProfileCard />
+    </div>
+  );
+}
+
+export default App;
