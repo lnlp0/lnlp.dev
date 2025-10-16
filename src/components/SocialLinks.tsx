5314857cf6e9073ba93c4e7ebae2a79986d73bb4
lnlp <chadg1212@naver.com>
2025-10-16 14:51:46 +0900

    feat :: 링크 수정

--- src/components/SocialLinks.tsx
+++ src/components/SocialLinks.tsx
@@ -15,7 +15,7 @@ const SocialLinks: React.FC<SocialLinksProps> = ({ isVisible }) => {
   const links: SocialLink[] = [
       { name: 'LinkedIn', icon: <Linkedin size={24} />, url: 'https://www.linkedin.com/in/lnlp' },
       { name: 'GitHub', icon: <Github size={24} />, url: 'https://github.com/lnlp0' },
-      { name: 'Portfolio', icon: <Globe size={24} />, url: 'https://portfolio.lnlp.dev' },
+      { name: 'Portfolio', icon: <Globe size={24} />, url: 'https://drive.google.com/file/d/1YdBqqnbJzRUrq3aiuMmAJMcspFyxBaMi/view?usp=sharing' },
       { name: 'Email', icon: <Mail size={24} />, url: 'mailto:me@lnlp.dev' },
       { name: 'Instagram', icon: <Instagram size={24} />, url: 'https://instagram.com/ckehdrb/' },
   ];
@@ -31,9 +31,9 @@ const SocialLinks: React.FC<SocialLinksProps> = ({ isVisible }) => {
           href={link.url}
           target="_blank"
           rel="noopener noreferrer"
-          className="flex flex-col items-center text-white hover:text-gray-300 transition-colors duration-300"
+          className="flex flex-col items-center text-white transition-colors duration-300 hover:text-gray-300"
         >
-          <div className="w-12 h-12 rounded-full border border-white flex items-center justify-center mb-2">
+          <div className="flex items-center justify-center w-12 h-12 mb-2 border border-white rounded-full">
             {link.icon}
           </div>
           <span className="text-sm font-light">{link.name}</span>
